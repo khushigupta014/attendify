0d6d83a36524f92234cfc651424fb8f825294715
karanprajapat824 <karanprajapat824@gmail.com>
2024-04-28 11:24:56 +0530

    hero update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import './App.css'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Hero from './Hero';
 
 function App() {
   
 
   return (
     <>
-      hello
+    <BrowserRouter >
+    <Routes >
+      <Route path='/' element={<Hero />} />
+    </Routes>
+</BrowserRouter>
     </>
   )
 }
---
+++ src/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Hero.css'
+const Hero = () => {
+  return (
+    <div className='body'>
+      <div className='form'>
+        <div className='title'>
+            Attendify
+            <div className='title1'>
+                Welcome,join Us
+            </div>
+        </div>
+        <div className='fields'>
+            <div>
+                
+            </div>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
